1de0c6b0c2e8d696c887e43c5674eeccac682291
berkiyo <berk@tekbyte.net>
2022-10-30 15:26:28 +1100

    Initial commit with core project files.

---
+++ Infinity/InfinityApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfinityApp.swift
+//  Infinity
+//
+//  Created by Berk Dogan on 30/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct InfinityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
