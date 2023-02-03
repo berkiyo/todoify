315bd4b37c6c709fb85624892d55a86b313f2dad
berkiyo <berk@tekbyte.net>
2023-02-03 16:36:53 +0300

    Added new changes

--- src/ContentView.swift
+++ src/ContentView.swift
@@ -5,79 +5,28 @@
  */
 
 import SwiftUI
+import StoreKit
+import CoreData
 
-/********************
- Struct for Tracking Activities
- */
-struct Task: Identifiable {
-    let id = UUID() // the identifier
-    let name: String // the name of the task
-    var isCompleted: Bool = false // whether it is completed or not
-}
-
-/********************
- Sections for the app
- */
-
-enum Sections: String, CaseIterable {
-    case pending = "Pending"
-    case completed = "Completed"
-}
-
-/********************
- Main view 
- 
- */
 struct ContentView: View {
     
-    // Hardcoded tasks, just for testing purposes
-    @State private var tasks = [Task(name: "Mow the lawn", isCompleted: true),
-                                Task(name:"Wash the car", isCompleted: false),
-                                Task(name: "Feed Piffin")]
+    // Properties
+    @State private var showingAddTodoView: Bool = false
     
-    // Property for pending tasks
-    var pendingTasks: [Binding<Task>] {
-        $tasks.filter { !$0.isCompleted.wrappedValue } // the ! indicates logical "NOT"
-    }
-
-    // Property for completed tasks
-    var completedTasks: [Binding<Task>] {
-        $tasks.filter { $0.isCompleted.wrappedValue } 
-    }
     
-    // MAIN BODY START
+    // Body
     var body: some View {
-        List {
-            ForEach(Sections.allCases, id: \.self) {
-                section in Section {
-                    ForEach(section == .pending ? pendingTasks: completedTasks) { $task in TaskViewCell(task: $task)} 
-                    // if section = pending, then display pending, else, display completed
-                } header: {
-                    Text(section.rawValue)
-                }
+        NavigationView {
+            List (0 ..< 5) {
+                item in Text("Hello World")
+            } // List
+            .navigationBarTitle("Todo", displayMode: .inline)
+            .navigationBarItems(trailing: Button(action: self.showingAddTodoView.toggle()  )) {
+                Image(systemName: "plus")
             }
-        }
-    }
-   
-}
-
-/********************
- TaskViewCell
- */
-struct TaskViewCell: View {
-    
-    @Binding var task: Task
-    
-    var body: some View {
-        HStack {
-            Image(systemName: task.isCompleted ? "checkmark.square": "square") // task.isComplete
-                .onTapGesture {
-                    withAnimation(.easeIn) {
-                        task.isCompleted.toggle()    
-                    }
-                }
-            Text(task.name)
-            
+            .sheet(isPresented: $showingAddTodoView) {
+                AddTodoView()
+            }
         }
     }
 }
---
+++ src/PlaceholderView.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+struct PLACEHOLDER: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
