ef068eda8d21753a3001d8f39f45f65f6be32597
berkiyo <berk@tekbyte.net>
2023-04-04 14:07:14 +1000

    Updates to date logic (ItemModel)

--- src/Models/ItemModel.swift
+++ src/Models/ItemModel.swift
@@ -10,18 +10,20 @@ struct ItemModel: Identifiable, Codable {
     let title: String           // name of streak
     var theDate: Int            // the number of days since streak started
     var isCompleted: Bool       // streak completed status
-    var theStartDate: Date      // the start date (i.e. when the streka was first started
+    var theStartDate: Date      // the start date (i.e. when the streak was first started
+    var overwrittenDate: Date = Date.now   // the overwritten latest date.
     var theColor: Int           // the color picked (array of 8 elements)
     
     
     // init
-    init(id: String = UUID().uuidString, title: String, isCompleted: Bool, theDate: Int, theStartDate: Date, theColor: Int) {
+    init(id: String = UUID().uuidString, title: String, isCompleted: Bool, theDate: Int, theStartDate: Date, theColor: Int, overwrittenDate: Date) {
         self.id = id
         self.title = title
         self.theDate = theDate
         self.isCompleted = isCompleted
         self.theStartDate = theStartDate
         self.theColor = theColor
+        self.overwrittenDate = overwrittenDate
     }
     
     // update the items
@@ -31,12 +33,13 @@ struct ItemModel: Identifiable, Codable {
         let timeToLive: TimeInterval = 60*60*24 // 60 seconds * 60 minutes * 24 hours
         
         let isExpired: Bool = Date().timeIntervalSince(theStartDate) >= timeToLive
+        //let isExpiredNew: Bool = Date().timeIntervalSince(overwrittenDate) >= timeToLive
         
         if isExpired {
             newDate = newDate + 1
-            return ItemModel(id: id, title: title, isCompleted: true, theDate: newDate, theStartDate: theStartDate, theColor: theColor)
+            return ItemModel(id: id, title: title, isCompleted: true, theDate: newDate, theStartDate: overwrittenDate, theColor: theColor, overwrittenDate: Date.now)
         } else {
-            return ItemModel(id: id, title: title, isCompleted: true, theDate: newDate, theStartDate: theStartDate, theColor: theColor)
+            return ItemModel(id: id, title: title, isCompleted: true, theDate: newDate, theStartDate: theStartDate, theColor: theColor, overwrittenDate: overwrittenDate)
         }
     }
 }
